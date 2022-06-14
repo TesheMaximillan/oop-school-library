2730f407ffaf5da16ea0d6636a0c3b733feb1f9c
Teshome Kurabachew <dear.teshe@gmail.com>
2022-06-14 14:21:45 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -1,11 +1,9 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name = "Unknown", parent_permission = true)
-    @age = age
-    @name = name
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
-    @parent_permission = parent_permission
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,12 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = "Unknown", parent_permission = true)
-    @age = age
-    @name = name
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
-    @parent_permission = parent_permission
   end
 
   def can_use_services?
     true
   end
 end
-
-puts Teacher.new(5, "Maths", "Abebe", false).can_use_services?
