02f262a311b0ea34076d9d2d7e4601062bfaf391
Teshome Kurabachew <dear.teshe@gmail.com>
2022-06-14 13:08:06 +0300

    Create Teacher class

---
+++ teacher.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = "Unknown",  parent_permission = true)
+    @age = age
+    @name = name
+    @specialization = specialization
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+puts Teacher.new(5, "Maths", "Abebe", false).can_use_services?
