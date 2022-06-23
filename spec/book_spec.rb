050a53edb114c32d72f5281a05d69180fb7d32c4
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-23 22:11:35 +0300

    Add book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,6 +8,14 @@ describe  "Book Tests" do
        it "should say 'Test Title' when we access Book Title" do 
           expect(@test_book.title) == "Test Title"
        end
+
+       it "should say 'Test Book Author' when we access Book Author" do 
+          expect(@test_book.author) == "Test Author"
+       end
        
+       it "should say 'Test Book Rentals to be empty before Renting' when we access Book Rentals" do 
+          expect(@test_book.rentals.empty?) 
+       end
+
     end
  end
